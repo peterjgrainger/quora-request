2e1f744c052a7395ec5ed694222f5fbc93ed0e7f
petergrainger <peter.grainger1@dwp.gsi.gov.uk>
2018-08-22 05:32:17 +0100

    add remote logging to rollbar

--- src/add-question.ts
+++ src/add-question.ts
@@ -1,6 +1,13 @@
 import { Context } from "koa";
 import {addQuestion} from 'quora-automation'
 import { DEPLOYMENT_TYPES } from 'quora-automation/js/deployment-types';
+import * as Rollbar from 'rollbar'
+
+const rollbar = new Rollbar({
+    accessToken: process.env.ROLLBAR_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true
+  });
 
 
 const deployment = process.env.DOCKER_DEPLOYMENT ? 
@@ -11,7 +18,8 @@ export async function addQuestionRoute(ctx:Context) {
     const result = await addQuestion(ctx.request.body.question, {
         username: process.env.USERNAME,
         password: process.env.PASSWORD,
-        deploymentType: deployment
+        deploymentType: deployment,
+        logger: rollbar
     })
 
     ctx.body = result
--- src/token-check.ts
+++ src/token-check.ts
@@ -2,8 +2,8 @@ import { Context } from "koa";
 
 export async function tokenCheck(ctx : Context, next: () => Promise<any>) {
     const authHeader = ctx.request.header.authorization
-    const token = authHeader.replace('Bearer ', '')
-    const validToken = new Buffer(token, 'base64').toString('ascii') === process.env.API_KEY
+    const token: string = authHeader.replace('Bearer ', '')
+    const validToken = Buffer.from(token, 'base64').toString('ascii') === process.env.API_KEY
     if(validToken) {
         await next();
     } else {
